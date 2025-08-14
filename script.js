40ccadd8afe7873ad6aeb1af1de947498a82098b
Sayan Paul <140426228+sayan-3002@users.noreply.github.com>
2025-08-14 23:01:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+const themeToggle = document.getElementById("theme-toggle");
+let darkMode = true;
+
+themeToggle.addEventListener("click", () => {
+    darkMode = !darkMode;
+    document.body.style.background = darkMode 
+        ? "linear-gradient(120deg, #1a1a1a, #222)" 
+        : "linear-gradient(120deg, #f3f3f3, #ffffff)";
+    document.body.style.color = darkMode ? "#fff" : "#000";
+    themeToggle.textContent = darkMode ? "🌙" : "🌞";
+});
+
+*/
